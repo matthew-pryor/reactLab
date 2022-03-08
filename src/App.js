477ea6204fa169ff6d4a92b3382cd99e179d66b5
matthew-pryor <pryor.matthew.a@gmail.com>
2022-03-08 09:03:46 -0600

    created displayentries componenet and instantiated component on app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
+import React, {useState} from "react";
+import DisplayEntries from "./Components/DisplayEntries/DisplayEntries";
 
 function App() {
+
+  const [entries, setEntries] = useState([{weight: 175, date: '11-23-2021'}, {weight: 178, date: '11-22-2021'}])
+
   return (
     <div>
-      <h3>Hello World</h3>
+      
+      <DisplayEntries parentEntries={entries}/>
 
     </div>
   );
